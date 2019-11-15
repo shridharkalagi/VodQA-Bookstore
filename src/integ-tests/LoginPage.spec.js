efd99637aff3bc4e5c1bb8fc92275fddcd433cd3
shridhar kalagi <shridharkalagi@gmail.com>
2019-11-15 14:32:37 +0530

    Add integ tests for login page

---
+++ src/integ-tests/LoginPage.spec.js
@@ -0,0 +1,33 @@
+import { fireEvent, waitForElement, render } from '@testing-library/react';
+import React from 'react'
+import LoginPage from '../components/LoginPage'
+import { Router } from 'react-router-dom'
+import { createMemoryHistory } from 'history'
+describe('Login page tests', () => {
+
+    it('validate the login Page UI', async () => {
+        const { getByLabelText, getByPlaceholderText, getByText } = renderLoginPage()
+        await waitForElement(() => getByText('Email'));
+        expect(getByText('Email')).toBeDefined();
+        expect(getByPlaceholderText('Password')).toBeDefined();
+        expect(getByText('Validate')).toBeDefined();
+        expect(getByText('Sign In')).toBeDefined();
+    })
+
+    it('validate the Error messages on clicking validate', async () => {
+        const { getByText } = renderLoginPage();
+        fireEvent.click(getByText('Validate'));
+        expect(getByText('Please provide a valid email.')).toBeDefined();
+        expect(getByText('Please provide a valid password. (atleast 1 char)')).toBeDefined();
+    })
+});
+
+
+
+function renderLoginPage() {
+    const history = createMemoryHistory();
+    return render(<Router history={history}>
+        <LoginPage></LoginPage>
+    </Router>);
+}
+
