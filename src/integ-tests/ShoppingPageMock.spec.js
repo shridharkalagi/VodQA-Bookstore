f970f3b11e238606cf2a330d8c045a786381d132
shridhar kalagi <shridharkalagi@gmail.com>
2019-11-16 08:45:27 +0530

    Add Shopping page Mock test

--- src/integ-tests/ShoppingPageMock.spec.js
+++ src/integ-tests/ShoppingPageMock.spec.js
@@ -4,15 +4,20 @@ import React from 'react'
 import ShoppingPage from '../components/VQStore'
 import { Router } from 'react-router-dom'
 import { createMemoryHistory } from 'history'
-
+import mock from './mock.json';
+jest.mock('axios');
+import axiosMock from 'axios';
 describe('Shopping Page tests', () => {
 
     it('validate the Shopping Page UI', async () => {
+        axiosMock.mockResolvedValueOnce({
+            data: mock,
+        });
         const { getAllByText, getByPlaceholderText, getByText ,queryByLabelText} = renderShoppingPage()
         await waitForElement(() => getByText('Filter Authors'));
         await waitForElement(() => queryByLabelText('Dan Brown'));
         fireEvent.click(queryByLabelText('Rujuta Divekar'));
-        expect(getByText('Indian Superfoods')).toBeDefined();
+        expect(getByText('Indian Superfoods Mock')).toBeDefined();
         expect(getAllByText('Add to Cart').length).toBe(1);
 
         expect(document.querySelectorAll('.card').length).toBe(1);
