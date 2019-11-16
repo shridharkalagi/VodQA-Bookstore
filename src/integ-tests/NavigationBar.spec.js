f0555d59ab29bda81ed5dd0ef1119b2d48a12aee
shridhar kalagi <shridharkalagi@gmail.com>
2019-11-16 08:21:03 +0530

    Add Shopping page tests

--- src/integ-tests/NavigationBar.spec.js
+++ src/integ-tests/NavigationBar.spec.js
@@ -5,40 +5,16 @@ import NavigationBar from '../components/NavContainer'
 import { Router } from 'react-router-dom'
 import { createMemoryHistory } from 'history'
 import { JestEnvironment } from '@jest/environment';
-import { authService } from '../services/authService' 
+import { authService } from '../services/authService'
 // import localStorage from global.localStorage;
 // jest.mock('localStorage');
 
 describe('Shopping Page tests', () => {
-    // beforeAll(() => {
-    //     class LocalStorage {
-    //         constructor() {
-    //           this.store = {};
-    //         }
-          
-    //         getItem(key) {
-    //           return this.store[key] || null;
-    //         }
-          
-    //         setItem(key, value) {
-    //           this.store[key] = value.toString();
-    //         }
-          
-    //         removeItem(key) {
-    //           delete this.store[key];
-    //         }
-          
-    //         reset() {
-    //           this.store = {};
-    //         }
-    //       };
-    //       global.localStorage = new LocalStorage;
-        // global.localStorage.setItem('isLoggedIn','true');
-    // });
-    it('validate the Shopping Page UI', async () =>  {
+
+    it('validate the Shopping Page UI - Authenticate', async () => {
         authService.authenticate();
-        localStorage.setItem('isLoggedIn', true);
-        console.log(localStorage.getItem('isLoggedIn'));
+        // global.localStorage.setItem('isLoggedIn', true);
+        // console.log(localStorage.getItem('isLoggedIn'));
         const { getByLabelText, getByPlaceholderText, getByText } = renderNavigationBar()
         await waitForElement(() => getByText('VodQA-Bookstore'));
         // fireEvent.click(getByText('Shop'));
@@ -46,16 +22,17 @@ describe('Shopping Page tests', () => {
         expect(getByText('Sign Out')).toBeDefined();
 
     })
+
+    it('validate the Shopping Page UI', async () => {
+        const { getByText } = renderNavigationBar()
+        expect(getByText('VodQA-Bookstore')).toBeDefined();
+        expect(getByText('Shop')).toBeDefined();
+    })
 });
 
 
 function renderNavigationBar() {
-    localStorage.setItem('isLoggedIn', true);
     return render(
-
-        // <LocalStorageMock items={{ isLoggedIn: 'true' }}>
-            <NavigationBar></NavigationBar>
-        // </LocalStorageMock>
-
+        <NavigationBar></NavigationBar>
     )
 }
--- src/integ-tests/ShoppingPage.spec.js
+++ src/integ-tests/ShoppingPage.spec.js
@@ -1,15 +1,21 @@
 import { LocalStorageMock } from '@react-mock/localstorage';
-import { fireEvent, waitForElement, render } from '@testing-library/react';
+import { fireEvent, waitForElement, render, getAllByText } from '@testing-library/react';
 import React from 'react'
 import ShoppingPage from '../components/VQStore'
 import { Router } from 'react-router-dom'
 import { createMemoryHistory } from 'history'
+
 describe('Shopping Page tests', () => {
 
     it('validate the Shopping Page UI', async () => {
-        const { getByLabelText, getByPlaceholderText, getByText } = renderShoppingPage()
+        const { getAllByText, getByPlaceholderText, getByText ,queryByLabelText} = renderShoppingPage()
         await waitForElement(() => getByText('Filter Authors'));
-        
+        await waitForElement(() => queryByLabelText('Dan Brown'));
+        fireEvent.click(queryByLabelText('Rujuta Divekar'));
+        expect(getByText('Indian Superfoods')).toBeDefined();
+        expect(getAllByText('Add to Cart').length).toBe(1);
+
+        expect(document.querySelectorAll('.card').length).toBe(1);
     })
 });
 
